7d81579689056bbe1007c3eda8566b917293cdf7
CallumGooding <cgooding.57963@farnborough.ac.uk>
2016-12-08 21:38:24 +0000

    08/12 j

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -16,6 +16,7 @@ function setup(){
 
 var is=[];
 var obs=[];
+var pwrs=[];
 var icount;
 
 function draw(){
@@ -26,22 +27,17 @@ function draw(){
         title();
         o=0;
     } else{
-        if(frameCount<250){
-            textSize(map(frameCount,200,250,width/5.5,55));
-        } else{
-            textSize(55);
-        }
         if(!lost){
             game();
         }else{
-            looseScreen();
+            loseScreen();
         }
 
     }
 
 }
 
-function looseScreen() {
+function loseScreen() {
     textAlign(CENTER,BOTTOM);
     textSize(width/6);
     text("YOU LOSE",width/2,height/2);
@@ -59,40 +55,28 @@ var j=0,f=0;
 var lost, lostFrame;
 function game() {
     o-=4;
-    v-=0.5;
+    v-=grav;
     y+=v;
     if(y<=0){
         y=0;
         v=0;
         j=0;
     }
+    textAlign(LEFT,BOTTOM);
+    var ly;
     if(frameCount<250){
-        var ly = map(frameCount,200,250,height,height/2+30);
-        line(0,ly,width,ly);
+        ly = map(frameCount,200,250,height,height/2+30);
+
+        textSize(40);
+        text("You have "+icount+" Is on your boi.",10,map(frameCount,200,250,height+50,height-10));
+        textSize(map(frameCount,200,250,width/5.5,55));
     }else{
-        line(0,height/2+30,width,height/2+30);
+        ly = height/2+30;
+        textSize(40);
+        text("You have "+icount+" Is on your boi.",10,height-10);
+        textSize(55);
     }
-    textAlign(CENTER,CENTER);
-    for(var i = 0; i<is.length;i++){
-        push();
-        translate(width/2-i*textWidth("I")+o,(height/2)-is[i].y);
-        rotate(is[i].rot);
-        text("I",0,0);
-        pop();
-    }
-    if(Math.random()<icount*0.0001){
-        obs[obs.length]=width;
-    }
-    for(var i = 0; i<obs.length;i++){
-        rect(obs[i],height/2-30,20,60);
-        if(obs[i]<width/2+10&&obs[i]>width/2-10){
-            if (y < 90) {
-                lost=true;
-                lostFrame = frameCount;
-            }
-        }
-        obs[i]-=3.8;
-    }
+    line(0,ly,width,ly);
     if(-o>=textWidth("I")){
         o=0;
         for(var i = is.length-2;i>=0;i--){
@@ -109,7 +93,76 @@ function game() {
 
         }
     }
+    textAlign(CENTER,CENTER);
+    for(var i = 0; i<is.length;i++){
+        push();
+        translate(width/2-i*textWidth("I")+o,(height/2)-is[i].y);
+        rotate(is[i].rot);
+        text("I",0,0);
+        pop();
+    }
+    if(Math.random()<icount*0.0001){
+        obs[obs.length]=width;
+    }
+    for(var i = 0; i<obs.length;i++){
+        rect(obs[i],ly-60,20,60);
+        if(obs[i]<width/2+10&&obs[i]>width/2-10){
+            if (y < 90) {
+                lost=true;
+                lostFrame = frameCount;
+            }
+        }
+        obs[i]-=3.8;
+        if(obs[i]<0){
+            obs.splice(i,1);
+        }
+    }
+    if(Math.random()<0.01){
+        pwrs[pwrs.length]={type:Math.round(Math.random()*2),x:width,y:Math.random()*height/2};
+    }
+    for(var i = 0; i<pwrs.length;i++){
+        pwrs[i].x-=3.8;
+        ellipse(pwrs[i].x,ly-pwrs[i].y,10,10);
+        if(pwrs[i].x<0){
+            pwrs.splice(i,1);
+        }else if(pwrs[i].x<width/2+15&&pwrs[i].x>width/2-15){
+            if (y+35>pwrs[i].y && y-35<pwrs[i].y) {
+                switch (pwrs[i].type){
+                    case 0: loGrav = 500;break;
+                    case 1: pwrJmp = 500;break;
+                    case 2: triJmp = 500;break;
+                }
+                pwrs.splice(i,1);
+            }
+        }
+    }
+    textAlign(RIGHT,TOP);
+    textSize(40);
+    if(loGrav>0){
+        loGrav--;
+        grav=0.4;
+        text("Low gravity",width,20);
+    }else{
+        grav=0.5;
+    }
+    if(pwrJmp>0){
+        pwrJmp--;
+        pwr=17;
+        text("Power jump",width,80);
+    }else{
+        pwr=13;
+    }
+    if(triJmp>0){
+        triJmp--;
+        jmpLim=3;
+        text("Triple jump",width,140);
+    }else{
+        jmpLim=2;
+    }
 }
+
+var loGrav=0, pwrJmp=0, triJmp=0;
+var grav = 0.5, pwr = 13, jmpLim = 2;
 
 function mousePressed(){
     if(lost){
@@ -117,11 +170,11 @@ function mousePressed(){
             location.reload();
         }
     }else{
-        if(j<2){
-            if(j==1){
+        if(j<jmpLim){
+            if(j>0){
                 f=1;
             }
-            v=13;
+            v=pwr;
             j++;
         }
     }
