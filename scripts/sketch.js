90717650db13e5065bb86947d64a012d5b10b26b
CallumGooding <cgooding.57963@farnborough.ac.uk>
2016-12-08 18:34:56 +0000

    08/12 g

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -5,14 +5,17 @@ function setup(){
     s = $("#sketch");
     c=createCanvas(s.width(),s.height());
     c.parent("sketch");
-    textSize(width/7);
+    textSize(55);
     icount = Math.round(width/2/textWidth("I"));
     for(var i = 0 ; i < icount;i++){
         is[i]={y:0,rot:0};
     }
+    obs[0] = width;
+    fill(0);
 }
 
 var is=[];
+var obs=[];
 var icount;
 
 function draw(){
@@ -24,19 +27,36 @@ function draw(){
         o=0;
     } else{
         if(frameCount<250){
-            textSize(width/map(frameCount,200,250,5.5,7));
+            textSize(map(frameCount,200,250,width/5.5,55));
         } else{
-            textSize(width/7);
+            textSize(55);
         }
-        game();
+        if(!lost){
+            game();
+        }else{
+            looseScreen();
+        }
+
     }
 
 }
+
+function looseScreen() {
+    textAlign(CENTER,CENTER);
+    textSize(200);
+    text("YOU LOSE",width/2,height/2);
+    textSize(100);
+    text("With "+icount+" Is on your boi.",width/2,height/2+150);
+    textSize(50);
+    text("Click to restart.",width/2,height-50);
+}
+
 var o;
 var y=0,v=0;
 var j=0,f=0;
+var lost, lostFrame;
 function game() {
-    o-=13;
+    o-=4;
     v-=0.5;
     y+=v;
     if(y<=0){
@@ -44,13 +64,32 @@ function game() {
         v=0;
         j=0;
     }
+    if(frameCount<250){
+        var ly = map(frameCount,200,250,height,height/2+30);
+        line(0,ly,width,ly);
+    }else{
+        line(0,height/2+30,width,height/2+30);
+    }
     textAlign(CENTER,CENTER);
     for(var i = 0; i<is.length;i++){
         push();
-        translate(width/2-i*textWidth("I")+o,height/2-is[i].y);
+        translate(width/2-i*textWidth("I")+o,(height/2)-is[i].y);
         rotate(is[i].rot);
         text("I",0,0);
         pop();
+    }
+    if(Math.random()<icount*0.0001){
+        obs[obs.length]=width;
+    }
+    for(var i = 0; i<obs.length;i++){
+        rect(obs[i],height/2-30,20,60);
+        if(obs[i]<width/2+10&&obs[i]>width/2-10){
+            if (y < 90) {
+                lost=true;
+                lostFrame = frameCount;
+            }
+        }
+        obs[i]-=3.8;
     }
     if(-o>=textWidth("I")){
         o=0;
@@ -58,6 +97,7 @@ function game() {
             is[i+1]=is[i];
         }
         is[0]={y:y,rot:0};
+        icount++;
         if(f>0){
             is[0].rot=TWO_PI/10*f;
             f++;
@@ -70,18 +110,20 @@ function game() {
 }
 
 function mousePressed(){
-    if(j<2){
-        if(j==1){
-            f=1;
+    if(lost){
+        if(frameCount>lostFrame+60) {
+            location.reload();
         }
-        v+=10;
-        j++;
+    }else{
+        if(j<2){
+            if(j==1){
+                f=1;
+            }
+            v=13;
+            j++;
+        }
     }
 }
-
-
-
-
 
 var boi=" BOI";
 function title(){
